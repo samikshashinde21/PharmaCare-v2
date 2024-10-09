f91d943fc4f26105942bc30783ec6ecc52fcebf1
samikshashinde21 <ssshinde.omk@gmail.com>
2024-10-10 01:48:44 +0530

    Modeling the Data

--- backend/seeder.js
+++ backend/seeder.js
@@ -30,6 +30,7 @@ const importData = async() => {
         await Product.insertMany(sampleProducts);
 
         console.log('Data Imported!'.green.inverse);
+        process.exit();
         
     } catch (error) {
         
