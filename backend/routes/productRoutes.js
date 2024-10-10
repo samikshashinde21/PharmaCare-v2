58bdbe1090361927754e624579c6c6587b95e101
samikshashinde21 <ssshinde.omk@gmail.com>
2024-10-10 22:06:23 +0530

    Getting Products from Actual Database

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,14 +2,25 @@ import express from 'express';
 
 const router = express.Router();
 import asyncHandler from '../middleware/asyncHandler';
+import Product from '../models/productModel';
 
 router.get('/',asyncHandler( async (req, res) => {
+    const products = await Product.find({});
     res.json(products);
 }));
 
+//to get single product
 router.get('/',asyncHandler( async (req,res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
+    const product = await Product.findById(req.params.id);
+
+    //if product found
+    if(product){
+
+        return  res.json(product);
+    }
+
+    //if not
+    res.status(404).json({message: 'Product not found'});
 }) );
 
 export default router;
