87c7845231d79515ae0d40200aaa700616abb2f8
samikshashinde21 <ssshinde.omk@gmail.com>
2024-10-13 20:01:38 +0530

    Added Loader Component

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,5 +1,6 @@
 import { Row, Col } from "react-bootstrap"
 import Product from "../components/Product"
+import Loader from "../components/Loader"
 import { useGetProductsQuery } from "../slices/productsApiSlice"
 
 const HomeScreen = () => {
@@ -9,7 +10,7 @@ const HomeScreen = () => {
     // handling isLoading and error
     <>
       {isLoading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
         <div>{error?.data?.message || error.error}</div>
       ) : (
