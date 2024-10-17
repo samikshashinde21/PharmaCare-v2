122aa18da0caca0e808dc4aa0678fee5b4ff07aa
samikshashinde21 <ssshinde.omk@gmail.com>
2024-10-17 17:40:24 +0530

    Email & Password Validation, Auth Middleware

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,24 @@
 import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 8000;
 
 connectDB(); // connect to MongoDB
 
 const app = express();
+
+//body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//Cookie parser middleware
+app.use(cookieParser());
 
 app.get('/' , (req, res) => {
     res.send('API is running...');
@@ -26,6 +35,7 @@ app.get('/api/products/:id', (req, res) => {
 }); */
 
 app.use('/api/products' , productRoutes);
+app.use('/api/users' , userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
